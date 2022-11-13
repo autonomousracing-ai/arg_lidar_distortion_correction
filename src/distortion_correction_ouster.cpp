5cd26ee64d69d276fb9360339ee65b85d6723ea2
Markus Schratter <markus.schratter@v2c2.at>
2022-11-13 19:57:17 +0100

    renamed general (default) and ouster executable

--- src/distortion_correction_ouster.cpp
+++ src/distortion_correction_ouster.cpp
@@ -19,7 +19,6 @@ ros::Publisher distortion_correction_points_pub;
 
 
 double delta_t_;
-bool timing_correction_;
 
 static void scan_imu_odom_callback(const sensor_msgs::PointCloud2::ConstPtr& input_pcl2,
         const geometry_msgs::TwistStamped::ConstPtr& input_twist){
@@ -39,31 +38,27 @@ static void scan_imu_odom_callback(const sensor_msgs::PointCloud2::ConstPtr& inp
   v.at(1) = v.at(0);
   v.at(0) = vehicle_v;
 
-  //calculate vehicle's angular velocity, also keep the previous value.
+  //read vehicle's angular velocity, also keep the previous value.
   double angular_vz = input_twist->twist.angular.z;
   omega.at(1) = omega.at(0);
   omega.at(0) = angular_vz;
+
+  //average linear and angular velocity.
+  double v_i = (v.at(0) + v.at(1))/2;
+  double omega_i = (omega.at(0) + omega.at(1))/2;
+  //reduce velocity as real travelled distance is larger than the assumed path between two pointcloud measurements
+  v_i = v_i*sin((omega_i*delta_t_)/2)/(omega_i*delta_t_/2);
 
   //counter to address the correct value. Ouster does not store values in rows and columns. Thus .at(column,row) or
   //.at(n), where n = column*width + row does not work!
   //Columns are stored row-wise, meaning the first row contains the width/height first columns.
   //Accessing the element can be achieved by .at(n) where n = element_cnt + j.
-  
+  int element_cnt = 0;
+  for(int i = 0; i < pointcloud_width; i++){
 
-  double scan_x, scan_y, scan_z;
-  for ( pcl::PointCloud<pcl::PointXYZI>::iterator it = scan.points.begin(); it != scan.points.end(); it++ ) {
-      //read point coordinates
-    scan_x = it->x;
-    scan_y = it->y;
-    scan_z = it->z;
-    double alpha = atan2(scan_y, scan_x);
-    if(alpha < 0){
-      alpha = alpha + 2*M_PI;
-    }
-
-    //average linear and angular velocity.
-    double v_i = (v.at(0) + v.at(1))/2;
-    double omega_i = (omega.at(0) + omega.at(1))/2;
+    //for each new ray position, calculate a new alpha (azimuth). Ouster starts at 360° and finishes at 0° in clockwise
+    //direction. Thus the angle is subtracted from 2pi.
+    double alpha = 2*M_PI - (double(i)/double(pointcloud_width))*2*M_PI;
 
     //define the position and the orientation of the new coordinate system
     double theta_i = 0*(M_PI/180);
@@ -78,15 +73,31 @@ static void scan_imu_odom_callback(const sensor_msgs::PointCloud2::ConstPtr& inp
     double x_alpha = x_i + delta_t_*v_i*(alpha/(2*M_PI))*cos(theta_i - delta_theta_alpha/2);
     double y_alpha = y_i + delta_t_*v_i*(alpha/(2*M_PI))*sin(theta_i - delta_theta_alpha/2);
 
+    for(int j = 0; j < pointcloud_height; j++){
+
+      //skip zero points
+      if(scan.points.at(element_cnt+j).x == 0 && scan.points.at(element_cnt+j).y == 0
+        && scan.points.at(element_cnt+j).z == 0){
+        continue;
+      }
+
+      //read point coordinates
+      double scan_x = scan.points.at(element_cnt+j).x;
+      double scan_y = scan.points.at(element_cnt+j).y;
+      double scan_z = scan.points.at(element_cnt+j).z;
+
       //perform rotation
       double scan_x_rot = scan_x * cos(theta_alpha) - scan_y * sin(theta_alpha);
       double scan_y_rot = scan_x * sin(theta_alpha) + scan_y * cos(theta_alpha);
       double scan_z_rot = scan_z;
 
       //overwrite old values with corrected values.
-      it->x = (x_alpha + scan_x_rot);
-      it->y = (y_alpha + scan_y_rot);
-      it->z = 0 + scan_z_rot;
+      scan.points.at(element_cnt+j).x = (x_alpha + scan_x_rot);
+      scan.points.at(element_cnt+j).y = (y_alpha + scan_y_rot);
+      scan.points.at(element_cnt+j).z = 0 + scan_z_rot;
+    }
+    //increase the counter as pointcloud_height elements were corrected successfully.
+    element_cnt = element_cnt + pointcloud_height;
   }
 
   pcl::PointCloud<pcl::PointXYZI>::Ptr scan_ptr(new pcl::PointCloud<pcl::PointXYZI>(scan));
@@ -94,11 +105,6 @@ static void scan_imu_odom_callback(const sensor_msgs::PointCloud2::ConstPtr& inp
   pcl::toROSMsg(*scan_ptr, msg);
 
   msg.header = input_pcl2->header;
-  if ( timing_correction_) {
-    //std::cout << "time original: " <<  msg.header.stamp  << std::endl;
-    msg.header.stamp = msg.header.stamp + ros::Duration(delta_t_);
-    //std::cout << "time modified: " <<  msg.header.stamp  << std::endl;
-  }
   distortion_correction_points_pub.publish(msg);
 }
 
@@ -111,9 +117,7 @@ int main(int argc, char** argv)
   ros::NodeHandle private_nh("~");
 
   int rotation_frequency = 0;
-  private_nh.param<int>("rotation_frequency", rotation_frequency, 0);
-  private_nh.param<bool>("timing_correction", timing_correction_, false);
-
+  private_nh.param<int>("rotation_frequency", rotation_frequency);
 
   if (rotation_frequency > 0) {
     delta_t_ = 1.0 / rotation_frequency;
@@ -121,7 +125,7 @@ int main(int argc, char** argv)
   else {
     delta_t_ = 0.05;
   }
-
+  
   // Publishers
   distortion_correction_points_pub = nh.advertise<sensor_msgs::PointCloud2>("points_out", 10);
 
